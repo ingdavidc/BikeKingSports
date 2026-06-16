339e8aedce5e3b7152c6b4cfe2fc115df8dcea27
Ing. David Carreño <ing.david@dctelematica.com>
2026-06-16 15:01:58 -0500

    Update logo to PNG format

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -4,7 +4,7 @@ import styles from './Logo.module.css';
 export default function Logo() {
   return (
     <div className={styles.logoContainer}>
-      <img src="/logo.jpeg" alt="Bike King Sports" className={styles.logoImage} />
+      <img src="/logo.png" alt="Bike King Sports" className={styles.logoImage} />
     </div>
   );
 }
